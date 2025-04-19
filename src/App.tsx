99d35c73aaaa7bc541f8f4ca24c4930ff7c00b4e
atimot <tmd6031@gmail.com>
2025-04-19 16:50:10 +0900

    fix timeline page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { AppRoutes } from './routes';
 
 export const App: React.FC = () => {
   return (
-    <div className="max-w-7xl mx-auto px-8 py-8 text-center">
-      <AppRoutes />
-    </div>
+    <AppRoutes />
   );
 };
