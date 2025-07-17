f4118a9920a580d0e1f1bf841b8a1ce96fd6edd6
atimot <tmd6031@gmail.com>
2025-07-17 09:38:36 +0900

    vite config修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://app:8080',
         changeOrigin: true,
         secure: false,
       },
